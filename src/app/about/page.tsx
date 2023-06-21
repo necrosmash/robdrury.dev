b903580262d3cb5f8dbed3c4af256e39dc41e04f
Rob Drury <robjdrewry@gmail.com>
2023-06-21 23:37:02 +0200

    bullet points tweak in about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,13 +9,15 @@ export default function About() {
           <br />
           For now, here are some bullet points:
         </div>
-        <li>I moved to Gothenburg in 2019</li>
-        <li>I have 6 years of professional experience as a developer</li>
-        <li>I have 2½ years of professional experience as a frontend developer, specifically</li>
-        <li>My main frontend experience is with React</li>
-        <li>I have studied (and should soon graduate from) a <em>Game Design & Technology</em> Master&apos;s at Gothenburg University</li>
-        <li>I have a very deaf, very white <a className="text-blue-500 hover:text-blue-700" target="_blank" href={"/sammy.jpg"}>cat named Sammy</a></li>
-        <li>I&apos;m currently <a className="text-blue-500 hover:text-blue-700" target="_blank" href={"/Rob_Drury_VisualCV_Resume.pdf"}>looking for work</a> 👀</li>
+        <ul className="list-disc ml-4">
+          <li>I moved to Gothenburg in 2019</li>
+          <li>I have 6 years of professional experience as a developer</li>
+          <li>I have 2½ years of professional experience as a frontend developer, specifically</li>
+          <li>My main frontend experience is with React</li>
+          <li>I have studied (and should soon graduate from) a <em>Game Design & Technology</em> Master&apos;s at Gothenburg University</li>
+          <li>I have a very deaf, very white <a className="text-blue-500 hover:text-blue-700" target="_blank" href={"/sammy.jpg"}>cat named Sammy</a></li>
+          <li>I&apos;m currently <a className="text-blue-500 hover:text-blue-700" target="_blank" href={"/Rob_Drury_VisualCV_Resume.pdf"}>looking for work</a> 👀</li>
+        </ul>
       </div>
       <div className="hidden sm:block min-w-[40%] self-center">
         <Image src="/wsm3.png" alt="will smith pose" width={400} height={689} />
