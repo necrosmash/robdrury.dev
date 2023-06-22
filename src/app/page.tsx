e0f94d268976be0e0e2a92974e719c4de6725ff4
Rob Drury <robjdrewry@gmail.com>
2023-06-22 11:45:04 +0200

    home page border tweaks

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -38,14 +38,14 @@ export default function Home() {
           <Image src="/portrait.jpeg" alt="Portrait photograph of Rob Drury" className="rounded-lg min-w-[120px] border-2 border-gray-700" width={204} height={304} />
         </a>
       </main>
-      <hr className="my-8 md:my-16" />
-      <div className="md:flex md:divide-x">
+      <hr className="my-8 md:my-16 border-gray-700 border-2"/>
+      <div className="md:flex md:divide-x-4 md:divide-gray-700">
         <div className="md:w-1/2 md:pr-8 mx-auto md:pb-0 pb-20 md:pb-0">
           <div className="mb-7 pb-5 md:mt-0 b-6 text-2xl md:text-3xl font-semibold text-center">
             Featured projects
           </div>
           <div className="flex items-center">
-            <Image className="border-2 border-white-700 rounded-lg border-2 border-gray-700" src="/mote banner.png" width={120} height={120} alt="Mote banner" />
+            <Image className="rounded-lg border-2 border-gray-700" src="/mote banner.png" width={120} height={120} alt="Mote banner" />
             <div className="ml-4">
               <a className="text-xl" href="https://necrosmash.itch.io/mote" target="_blank">
                 Mote
@@ -60,7 +60,7 @@ export default function Home() {
             <HiArrowRight className="ml-2 inline" size={16}/>
           </Link>
         </div>
-        <hr className="my-8 md:hide" />
+        <div className="my-8 md:hidden border-gray-700 border-2" />
         <div className="md:w-1/2 pb-20 md:pb-0 md:pl-8 mx-auto">
           <div className="pb-5 text-2xl md:text-3xl font-semibold text-center">
             Most recent posts
