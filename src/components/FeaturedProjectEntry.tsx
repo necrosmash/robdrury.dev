e7d595faae6cd4105ed83c5a16eb5bf254b6e686
Rob Drury <robjdrewry@gmail.com>
2023-08-01 15:51:06 +0200

    refactors Featured Projects into its own component

---
+++ src/components/FeaturedProjectEntry.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import TagsList from "@/components/TagsList"
+
+export default function FeaturedProjectsEntry({ imagePath, imageAlt, hrefPath, title, blurb, tags, width = 125, height = 125 }: ProjectEntry) {
+  return (
+    <div className="mt-7 flex items-center">
+      <Image className="rounded-lg border-2 border-gray-700" src={imagePath} width={width} height={height} alt={imageAlt} />
+      <div className="ml-4">
+        <a className="text-xl" href={hrefPath} target="_blank">
+          {title}
+        </a>
+        <div className="mt-1">
+          {blurb}
+        </div>
+        {tags && (
+          <TagsList tags={tags} />
+        )}
+      </div>
+    </div>
+  )
+}
